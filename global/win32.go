632fd27c01ac060118eb9943bb1ddf2f4f8e561e
Wafula-Ian01 <wafula-ian01@github.com>
2024-11-19 11:57:43 +0300

    managing little endianness

--- global/win32.go
+++ global/win32.go
@@ -1,8 +1,10 @@
 package global
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"math"
 )
 
 //Contains Microsoft Windows specific code
@@ -10,12 +12,14 @@ import (
 // S1 signed data datatypes
 type S1 = byte  //represents a char
 type S2 = int16 //represents a short
-type S4 = int64 //represents a long
+type S4 = int32 //represents a long
+type S8 = int64 //represents long long __int64
 
 // U1 unsigned data datatypes
-type U1 = byte  //represents a char
-type U2 = int16 //represents a short
-type U4 = int64 //represents a long
+type U1 = byte   //represents a char
+type U2 = uint16 //represents a short
+type U4 = uint32 //represents a long
+type U8 = uint64 //represents long long or __uint64
 
 // F4 float and double
 type F4 = float32 //represents float
@@ -45,11 +49,125 @@ func checkEndian() { //Checks for the endianness of the platform running the VM
 	return
 } //End of checking Endianness
 
-// converting big endianness to little endianness
-func byteCodeToWord(bytes []byte) byte {
-	var word U2
-	var buffer *U1
-	buffer = (*U1) & word
+func byteCodeToWord(bytes []U1) U2 {
+	if len(bytes) != 2 {
+		panic("Input slice must contain 2 bytes")
+	}
+	word := U2(bytes[1]) | U2(bytes[0])<<8
+
+	return word
+
 }
 
-//converting little endianness to big endianness
+func byteCodeToDword(bytes []U1) U4 {
+	if len(bytes) != 4 {
+		panic("Input slice must contain 4 bytes")
+	}
+
+	dword := U4(bytes[3]) | U4(bytes[2])<<8 | U4(bytes[1])<<16 | U4(bytes[0])<<24
+
+	return dword
+}
+
+func byteCodeToQword(bytes []U1) U8 {
+	if len(bytes) != 8 {
+		panic("Input slice must contain 8 bytes")
+	}
+
+	qword := U8(bytes[7]) | U8(bytes[6])<<8 | U8(bytes[5])<<16 | U8(bytes[4])<<24 | U8(bytes[3])<<32 |
+		U8(bytes[2])<<40 | U8(bytes[1])<<48 | U8(bytes[0])<<56
+
+	return qword
+}
+
+func byteCodeToFloat(bytes []U1) F4 {
+	if len(bytes) != 4 {
+		panic("Input slice must contain 4 bytes")
+	}
+	//reverse splice
+	reversedBytes := []byte{bytes[3], bytes[2], bytes[1], bytes[0]}
+
+	//convert bytes to uint32
+	bits := binary.LittleEndian.Uint32(reversedBytes)
+	//convert to float32
+	floatValue := math.Float32frombits(bits)
+
+	return floatValue
+}
+
+func byteCodeToDouble(bytes []U1) F8 {
+	if len(bytes) != 8 {
+		panic("Input slice must contain 8 bytes")
+	}
+
+	reverseBytes := []byte{bytes[7], bytes[6], bytes[5], bytes[4],
+		bytes[3], bytes[2], bytes[1], bytes[0]}
+
+	bits := binary.LittleEndian.Uint64(reverseBytes)
+
+	doubleValue := math.Float64frombits(bits)
+
+	return doubleValue
+}
+func wordToByteCode(word U2) []U1 {
+	arr := make([]U1, 2)
+
+	arr[0] = U1(word >> 8)
+	arr[1] = U1(word & 0xFF)
+
+	return arr
+}
+
+func dwordToByteCode(dword U4) []U1 {
+	arr := make([]U1, 4)
+
+	arr[0] = U1(dword & 0xFF)
+	arr[1] = U1((dword >> 8) & 0xFF)
+	arr[2] = U1((dword >> 16) & 0xFF)
+	arr[3] = U1((dword >> 24) & 0xFF)
+
+	return arr
+}
+
+func qwordToByteCode(qword U8) []U1 {
+	arr := make([]U1, 8)
+
+	arr[0] = U1(qword & 0xFF)
+	arr[1] = U1((qword >> 8) & 0xFF)
+	arr[2] = U1((qword >> 16) & 0xFF)
+	arr[3] = U1((qword >> 24) & 0xFF)
+	arr[4] = U1((qword >> 32) & 0xFF)
+	arr[5] = U1((qword >> 40) & 0xFF)
+	arr[6] = U1((qword >> 48) & 0xFF)
+	arr[7] = U1((qword >> 56) & 0xFF)
+
+	return arr
+}
+
+func floatToByteCode(floatValue F4) []U1 {
+	bits := math.Float32bits(floatValue)
+
+	buff := new(bytes.Buffer)
+
+	err := binary.Write(buff, binary.LittleEndian, bits)
+
+	if err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+
+	return buff.Bytes()
+}
+
+func doubleToByteCode(doubleValue F8) []U1 {
+	bits := math.Float64bits(doubleValue)
+
+	buff := new(bytes.Buffer) // holds the byte sequence
+
+	err := binary.Write(buff, binary.LittleEndian, bits)
+
+	if err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+
+	return buff.Bytes()
+}
